a5430818ada4f905a4b8e55afc639275216c84ad
Daniel Penelva <d4n.andrade@gmail.com>
2025-06-07 23:10:19 -0300

    feat: criando projeto spring boot e data jpa

---
+++ src/main/java/com/api/demo_data_jpa/DemoDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.api.demo_data_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/demo_data_jpa/DemoDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.demo_data_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
