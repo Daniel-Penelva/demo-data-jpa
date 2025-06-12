af92c2bf482caecf8540e11a678c555c05bb2547
Daniel Penelva <d4n.andrade@gmail.com>
2025-06-11 23:02:28 -0300

    feat: (funcionalidade: relação BIDIRECIONAL @onetomany entre  Lecture e Resource) Alterando a classe Resource para tornar o relacionamento bidirecional.
    Para ser bidirecional usa a propriedade mappedBy na classe Lecture. Aqui, para acessar a Lecture, você pode acessar o Resource diretamente através da propriedade lecture ou acessar a Lecture através do Resource, pois a relação é bidirecional.

--- src/main/java/com/api/demo_data_jpa/model/Resource.java
+++ src/main/java/com/api/demo_data_jpa/model/Resource.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -28,5 +29,14 @@ public class Resource {
 
     private int size;
     private String url;
+
+    // É uma relação BIDIRECIONAL OneToOne entre Resource e Lecture - para ser bidirecional usa a propriedade mappedBy na classe Lecture. Aqui, para acessar a Lecture, você pode acessar o Resource diretamente através da propriedade lecture ou acessar a Lecture através do Resource, pois a relação é bidirecional. 
+    // O "One" representa a entidade principal (Resource) e "One" representa a entidade dependente (Lecture).
+    // O recurso (Resource) é o dono da relação, pois ela tem a anotação @OneToOne na classe Palestra (Lecture).
+    // mappedBy = "resource" indica que a propriedade resource na classe Lecture é responsável por mapear essa relação.
+    // A relação é OneToOne, então um recurso pode pertencer a uma única palestra, e uma palestra pode ter um único recurso.
+    // Como se lê: Um recurso pode pertencer a uma única palestra, e uma palestra pode ter um único recurso.
+    @OneToOne(mappedBy = "resource")
+    private Lecture lecture;
     
 }
